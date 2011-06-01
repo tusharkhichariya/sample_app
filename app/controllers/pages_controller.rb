aae87d78126bfaa409dd055684d86d31597f0690
tushar.k <tushar@motifworks.com>
2011-06-01 20:10:40 +0530

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+	  @title = "Home Test"
   end
 
   def contact
+	  @title = "Contact Test"
+  end
+
+  def about
+	  @title = "About Test"
   end
 
 end
